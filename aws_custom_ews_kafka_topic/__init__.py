96bea6621bb7cd345d0e4d364a4ff658660060b6
John Preston <john@ews-network.net>
2021-04-06 17:43:17 +0100

    Bump to 0.0.3

--- aws_custom_ews_kafka_topic/__init__.py
+++ aws_custom_ews_kafka_topic/__init__.py
@@ -11,7 +11,7 @@ from troposphere.validators import positive_integer, boolean
 
 __author__ = """John Mille"""
 __email__ = "john@ews-network.net"
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 
 COMMON_PROPS = {
--- setup.py
+++ setup.py
@@ -70,6 +70,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/johnpreston/aws_custom_ews_kafka_topic",
-    version="0.0.2",
+    version="0.0.3",
     zip_safe=False,
 )
