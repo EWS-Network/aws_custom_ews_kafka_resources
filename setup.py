dfbcee6503c24e21f133047ed016f721255cb6ca
John Preston <john@ews-network.net>
2021-04-30 11:07:30 +0100

    Bump to 0.0.6

--- setup.py
+++ setup.py
@@ -70,6 +70,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/johnpreston/aws_custom_ews_kafka_topic",
-    version="0.0.5",
+    version="0.0.6",
     zip_safe=False,
 )
