c876c09a14c692cf10337022caee761fd321b158
John Preston <john@ews-network.net>
2021-04-30 11:47:53 +0100

    Bump to 0.0.7

--- aws_custom_ews_kafka_resources/__init__.py
+++ aws_custom_ews_kafka_resources/__init__.py
@@ -8,7 +8,7 @@ from troposphere import AWSProperty
 
 __author__ = """John Mille"""
 __email__ = "john@ews-network.net"
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 
 class KafkaAclPolicy(AWSProperty):
